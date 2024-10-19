faa242d3faf55571cafcf1a4288a310cefb044ca
Rustam Karimjonov <rustamkarimjonov19072007@gmail.com>
2024-10-19 11:59:05 +0500

    updated router link /main to/

--- src/router/index.js
+++ src/router/index.js
@@ -11,9 +11,10 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue'),
       children: [
         {
-          path: '/main',
+          path: '/',
           name: 'main',
           // route level code-splitting
+
           // this generates a separate chunk (About.[hash].js) for this route
           // which is lazy-loaded when the route is visited.
           component: () => import('../views/MainView.vue')
